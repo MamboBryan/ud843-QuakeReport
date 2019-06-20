d6d9d07b6ff4c55f1c1af37f48d30e6ccba918f5
MamboBryan <mambobryan@gmail.com>
2019-06-20 07:47:34 +0300

    Added Queries and functionality

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -3,29 +3,29 @@ package com.example.android.quakereport;
 public class Earthquake {
 
     //This is the variable name for the earthquake magnitude
-    private String mMagnitude;
+    private double mMagnitude;
 
     //This is the variable name for the earthquake location
     private String mLocation;
 
     //This is the variable name for the earthquake date
-    private String mDate;
+    private long mTimeInMilliseconds;
 
-    public Earthquake(String magnitude, String location, String date) {
+    public Earthquake(double magnitude, String location, long date) {
         mMagnitude = magnitude;
         mLocation = location;
-        mDate = date;
+        mTimeInMilliseconds = date;
     }
 
-    public String getMagnitude() {
+    public double getMagnitude() {
         return mMagnitude;
+    }
+
+    public long getDate() {
+        return mTimeInMilliseconds;
     }
 
     public String getLocation() {
         return mLocation;
-    }
-
-    public String getDate() {
-        return mDate;
     }
 }
