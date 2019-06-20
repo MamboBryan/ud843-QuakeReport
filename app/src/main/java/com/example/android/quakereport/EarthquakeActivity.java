5a0fa2a2395565c25eff1cb32c527e96ff5dcb9a
MamboBryan <mambobryan@gmail.com>
2019-06-20 05:31:33 +0300

    Created develop branch

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
+    
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
     @Override
